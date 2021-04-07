b300bddca6f6d9de3ffe833334f2376cada1dcb2
Iris <iris.souza@zup.com.br>
2021-04-07 15:57:09 -0300

    Adicionar model Saldo

---
+++ src/main/java/com/br/zup/zupnancas/entities/Saldo.java
@@ -0,0 +1,38 @@
+package com.br.zup.zupnancas.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Saldo {
+
+    private String cpf;
+    private double valor;
+    private double limite;
+
+    public Saldo() {
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+}
