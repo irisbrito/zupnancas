de439881fbe19b8c939565b35870a6593549dd56
Iris <iris.souza@zup.com.br>
2021-04-07 16:36:18 -0300

    Criar método para adicionar Saldo

---
+++ src/main/java/com/br/zup/zupnancas/controllers/SaldoController.java
@@ -0,0 +1,22 @@
+package com.br.zup.zupnancas.controllers;
+
+import com.br.zup.zupnancas.entities.Saldo;
+import com.br.zup.zupnancas.services.SaldoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("saldos/")
+public class SaldoController {
+
+    @Autowired
+    private SaldoService saldoService;
+
+    @PostMapping
+    public Saldo adicionarSaldo(@RequestBody Saldo saldo){
+        return saldoService.adicionarSaldo(saldo);
+    }
+}
