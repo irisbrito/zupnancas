b7f2d00711fd18c2b00fcd4d036686bc06fe546b
Iris <iris.souza@zup.com.br>
2021-04-07 16:14:31 -0300

    Criar classe categoria

---
+++ src/main/java/com/br/zup/zupnancas/entities/Categoria.java
@@ -0,0 +1,32 @@
+package com.br.zup.zupnancas.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+
+    public Categoria() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
