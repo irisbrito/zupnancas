c3e237b2a00984a91913f0b03c7c26abf164ac84
Iris <iris.souza@zup.com.br>
2021-04-07 16:28:51 -0300

    Criar ContaRepository

---
+++ src/main/java/com/br/zup/zupnancas/repositories/ContaRepository.java
@@ -0,0 +1,7 @@
+package com.br.zup.zupnancas.repositories;
+
+import com.br.zup.zupnancas.entities.Conta;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContaRepository extends CrudRepository<Conta,Integer> {
+}
