bdb975c4e3c1e81e8ddca617ec487b14c68b363f
Iris <iris.souza@zup.com.br>
2021-04-07 15:58:17 -0300

    Adicionar chave primária em Saldo

--- src/main/java/com/br/zup/zupnancas/entities/Saldo.java
+++ src/main/java/com/br/zup/zupnancas/entities/Saldo.java
@@ -1,10 +1,15 @@
 package com.br.zup.zupnancas.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Saldo {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String cpf;
     private double valor;
     private double limite;
