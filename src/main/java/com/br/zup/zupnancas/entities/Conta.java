308f5c8bd68c7ecb0d6949799f0fcbe0739ad54b
Iris <iris.souza@zup.com.br>
2021-04-07 16:06:47 -0300

    Adicionar entidade Conta

---
+++ src/main/java/com/br/zup/zupnancas/entities/Conta.java
@@ -0,0 +1,70 @@
+package com.br.zup.zupnancas.entities;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "contas")
+public class Conta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private double valor;
+    private String descricao;
+    private LocalDate dataDeEntrada;
+    private LocalDate dataDeVencimento;
+    private Status status;
+
+    public Conta() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataDeEntrada() {
+        return dataDeEntrada;
+    }
+
+    public void setDataDeEntrada(LocalDate dataDeEntrada) {
+        this.dataDeEntrada = dataDeEntrada;
+    }
+
+    public LocalDate getDataDeVencimento() {
+        return dataDeVencimento;
+    }
+
+    public void setDataDeVencimento(LocalDate dataDeVencimento) {
+        this.dataDeVencimento = dataDeVencimento;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
