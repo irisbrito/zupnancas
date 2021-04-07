76365cba58d02825bf68c2d4104204060cc31175
Iris <iris.souza@zup.com.br>
2021-04-07 16:28:36 -0300

    Criar CreditoRepository

---
+++ src/main/java/com/br/zup/zupnancas/repositories/CreditoRepository.java
@@ -0,0 +1,7 @@
+package com.br.zup.zupnancas.repositories;
+
+import com.br.zup.zupnancas.entities.Credito;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CreditoRepository extends CrudRepository<Credito,Integer> {
+}
