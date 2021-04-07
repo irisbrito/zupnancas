de86752fdf05297d1c9071f55d5553f0d305b479
Iris <iris.souza@zup.com.br>
2021-04-07 17:38:59 -0300

    Adicionar método de visualizar saldo

--- src/main/java/com/br/zup/zupnancas/controllers/SaldoController.java
+++ src/main/java/com/br/zup/zupnancas/controllers/SaldoController.java
@@ -3,10 +3,9 @@ package com.br.zup.zupnancas.controllers;
 import com.br.zup.zupnancas.entities.Saldo;
 import com.br.zup.zupnancas.services.SaldoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("saldos/")
@@ -18,6 +17,11 @@ public class SaldoController {
     @PostMapping
     public Saldo adicionarSaldo(@RequestBody Saldo saldo){
         return saldoService.adicionarSaldo(saldo);
+    }
+
+    @GetMapping("{cpf}/")
+    public Optional<Saldo> visualizarSaldo(@PathVariable String cpf){
+        return saldoService.visualizarSaldo(cpf);
     }
 
 }
--- src/main/java/com/br/zup/zupnancas/services/SaldoService.java
+++ src/main/java/com/br/zup/zupnancas/services/SaldoService.java
@@ -1,10 +1,11 @@
 package com.br.zup.zupnancas.services;
 
-import com.br.zup.zupnancas.entities.Credito;
 import com.br.zup.zupnancas.entities.Saldo;
 import com.br.zup.zupnancas.repositories.SaldoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class SaldoService {
@@ -14,6 +15,10 @@ public class SaldoService {
 
     public Saldo adicionarSaldo(Saldo saldo){
        return saldoRepository.save(saldo);
+    }
+
+    public Optional<Saldo> visualizarSaldo(String cpf){
+        return saldoRepository.findById(cpf);
     }
 
 }
