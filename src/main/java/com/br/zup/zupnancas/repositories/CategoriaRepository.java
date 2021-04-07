3364c80b7a90e0b257d3292a15c362ce63c8372b
Iris <iris.souza@zup.com.br>
2021-04-07 16:29:07 -0300

    Criar CategoriaRepository

---
+++ src/main/java/com/br/zup/zupnancas/repositories/CategoriaRepository.java
@@ -0,0 +1,7 @@
+package com.br.zup.zupnancas.repositories;
+
+import com.br.zup.zupnancas.entities.Categoria;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoriaRepository extends CrudRepository<Categoria,Integer> {
+}
