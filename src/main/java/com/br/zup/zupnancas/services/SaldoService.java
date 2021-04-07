4425d3d3f58933f12339e1e7abdc060264432031
Iris <iris.souza@zup.com.br>
2021-04-07 16:33:45 -0300

    Criar SaldoService

---
+++ src/main/java/com/br/zup/zupnancas/services/SaldoService.java
@@ -0,0 +1,17 @@
+package com.br.zup.zupnancas.services;
+
+import com.br.zup.zupnancas.entities.Saldo;
+import com.br.zup.zupnancas.repositories.SaldoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SaldoService {
+
+    @Autowired
+    private SaldoRepository saldoRepository;
+
+    public Saldo adicionarSaldo(Saldo saldo){
+       return saldoRepository.save(saldo);
+    }
+}
