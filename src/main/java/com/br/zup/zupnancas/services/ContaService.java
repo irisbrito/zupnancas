8c82cbf4e08640a4b0095e10ea9ef0d02cd2ed60
Iris <iris.souza@zup.com.br>
2021-04-08 14:12:14 -0300

    Adicionar validação para não cadastrar conta paga

--- src/main/java/com/br/zup/zupnancas/services/ContaService.java
+++ src/main/java/com/br/zup/zupnancas/services/ContaService.java
@@ -1,9 +1,12 @@
 package com.br.zup.zupnancas.services;
 
 import com.br.zup.zupnancas.entities.Conta;
+import com.br.zup.zupnancas.enums.Status;
 import com.br.zup.zupnancas.repositories.ContaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class ContaService {
@@ -12,6 +15,14 @@ public class ContaService {
     private ContaRepository contaRepository;
 
     public Conta cadastrarConta(Conta conta){
+        conta.setDataDeEntrada(LocalDate.now());
+        validarStatusDaConta(conta);
         return contaRepository.save(conta);
+    }
+
+    public void validarStatusDaConta(Conta conta) {
+        if (conta.getStatus() == Status.PAGO) {
+            throw new RuntimeException("Não é possível cadastrar uma conta paga");
+        }
     }
 }
