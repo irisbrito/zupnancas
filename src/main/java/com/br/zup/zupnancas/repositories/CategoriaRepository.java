a25bfb2737146ccded9f33761278f3f2173be569
Iris <iris.souza@zup.com.br>
2021-04-09 19:03:55 -0300

    Criar método findByNome

--- src/main/java/com/br/zup/zupnancas/repositories/CategoriaRepository.java
+++ src/main/java/com/br/zup/zupnancas/repositories/CategoriaRepository.java
@@ -3,5 +3,9 @@ package com.br.zup.zupnancas.repositories;
 import com.br.zup.zupnancas.entities.Categoria;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoriaRepository extends CrudRepository<Categoria,Integer> {
+    Optional<Categoria> findByNome(String nome);
+    Iterable<Categoria> findAllByNome(Categoria categoria);
 }
