fb3d4c03093e73b3195a881979866743237744fe
Iris <iris.souza@zup.com.br>
2021-04-09 20:49:36 -0300

    Adicionar documentação na SaldoService

--- src/main/java/com/br/zup/zupnancas/services/SaldoService.java
+++ src/main/java/com/br/zup/zupnancas/services/SaldoService.java
@@ -15,10 +15,20 @@ public class SaldoService {
     @Autowired
     private SaldoRepository saldoRepository;
 
+    /**
+     * Método para adicionar saldo
+     * @param saldo
+     * @return saldo
+     */
     public Saldo adicionarSaldo(Saldo saldo){
        return saldoRepository.save(saldo);
     }
 
+    /**
+     * Método de pesquisa de saldo pelo cpf
+     * @param cpf
+     * @return saldo
+     */
     public Saldo pesquisarSaldo(String cpf){
         Optional<Saldo> optionalSaldo = saldoRepository.findById(cpf);
 
@@ -29,6 +39,10 @@ public class SaldoService {
         return optionalSaldo.get();
     }
 
+    /**
+     * Método para adicionar o valor de um novo crédito no saldo
+     * @param credito
+     */
     public void creditarSaldo(Credito credito) {
         Saldo saldo = pesquisarSaldo(credito.getSaldo().getCpf());
         Double valorAtualizado = saldo.getValor() + credito.getValor();
@@ -37,6 +51,11 @@ public class SaldoService {
         saldoRepository.save(saldo);
     }
 
+    /**
+     * Método para debitar um valor do saldo de acordo com o valor pago na conta
+     * @param conta
+     * @return boolean
+     */
     public boolean debitar(Conta conta){
         Saldo saldo = pesquisarSaldo(conta.getSaldo().getCpf());
         double novoSaldo = saldo.getValor() + saldo.getLimite();
