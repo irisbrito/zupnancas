cf0d85cba9688134bfa8d765dc6d9a96073395b5
Iris <iris.souza@zup.com.br>
2021-04-14 20:40:09 -0300

    Adicionar classe de exceção conta não encontrada

---
+++ src/main/java/com/br/zup/zupnancas/exceptions/ContaNaoEncontradaException.java
@@ -0,0 +1,45 @@
+package com.br.zup.zupnancas.exceptions;
+
+public class ContaNaoEncontradaException extends RuntimeException {
+
+    private int Status = 400;
+    private String campo = "id";
+    private String razaoDoErro = "Bad request";
+    private String tipoDoErro = "Conta não encontrada";
+
+    public ContaNaoEncontradaException(String message) {
+        super(message);
+    }
+
+    public int getStatus() {
+        return Status;
+    }
+
+    public void setStatus(int status) {
+        Status = status;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public void setCampo(String campo) {
+        this.campo = campo;
+    }
+
+    public String getRazaoDoErro() {
+        return razaoDoErro;
+    }
+
+    public void setRazaoDoErro(String razaoDoErro) {
+        this.razaoDoErro = razaoDoErro;
+    }
+
+    public String getTipoDoErro() {
+        return tipoDoErro;
+    }
+
+    public void setTipoDoErro(String tipoDoErro) {
+        this.tipoDoErro = tipoDoErro;
+    }
+}
