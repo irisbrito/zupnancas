2c9a49464d1b6c26f9320a202697763898c65c0b
Iris <iris.souza@zup.com.br>
2021-04-14 19:14:49 -0300

    Adicionar manipulador de excecoes

---
+++ src/main/java/com/br/zup/zupnancas/exceptions/ManipuladorDeExcecoes.java
@@ -0,0 +1,29 @@
+package com.br.zup.zupnancas.exceptions;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ManipuladorDeExcecoes extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+        List<ObjetoDeErro> objetosDeErro = getErros(ex);
+        RespostaDeErro respostaDeErro = new RespostaDeErro("Validação", status.value(),
+                status.getReasonPhrase(), objetosDeErro);
+        return ResponseEntity.status(status).body(respostaDeErro);
+    }
+
+
+}
