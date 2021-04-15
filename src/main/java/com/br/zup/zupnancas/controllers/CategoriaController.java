65d5b7718d7e4cb960167f5767c1dcaccc8b5549
Iris <iris.souza@zup.com.br>
2021-04-15 12:39:15 -0300

    Adicionar @ResponseStatus em CategoriaController

--- src/main/java/com/br/zup/zupnancas/controllers/CategoriaController.java
+++ src/main/java/com/br/zup/zupnancas/controllers/CategoriaController.java
@@ -3,6 +3,7 @@ package com.br.zup.zupnancas.controllers;
 import com.br.zup.zupnancas.entities.Categoria;
 import com.br.zup.zupnancas.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,21 +14,25 @@ public class CategoriaController {
     private CategoriaService categoriaService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Categoria cadastrarCategoria(@RequestBody Categoria categoria){
         return categoriaService.cadastrarCategoria(categoria);
     }
 
     @GetMapping("{id}/")
+    @ResponseStatus(HttpStatus.OK)
     public Categoria pesquisarCategoriaPorId(@PathVariable int id){
         return categoriaService.buscarCategoriaPeloId(id);
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public Iterable<Categoria> listarCategorias(){
         return categoriaService.listarCategorias();
     }
 
     @DeleteMapping
+    @ResponseStatus(HttpStatus.OK)
     public void deletarCategoria(@PathVariable int id){
         categoriaService.deletarCategoria(id);
     }
