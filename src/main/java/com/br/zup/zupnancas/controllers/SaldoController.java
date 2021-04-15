3a08679352cc2ca2a60aee6d736dfdcabc281c3c
Iris <iris.souza@zup.com.br>
2021-04-15 12:37:45 -0300

    Adicionar @ResponseStatus em SaldoController

--- src/main/java/com/br/zup/zupnancas/controllers/SaldoController.java
+++ src/main/java/com/br/zup/zupnancas/controllers/SaldoController.java
@@ -3,6 +3,7 @@ package com.br.zup.zupnancas.controllers;
 import com.br.zup.zupnancas.entities.Saldo;
 import com.br.zup.zupnancas.services.SaldoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,11 +14,13 @@ public class SaldoController {
     private SaldoService saldoService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Saldo adicionarSaldo(@RequestBody Saldo saldo){
         return saldoService.adicionarSaldo(saldo);
     }
 
     @GetMapping("{cpf}/")
+    @ResponseStatus(HttpStatus.OK)
     public Saldo visualizarSaldo(@PathVariable String cpf){
         return saldoService.pesquisarSaldo(cpf);
     }
